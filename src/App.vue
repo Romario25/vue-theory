f975f1e84e28cccff426e275d663d21f2ed01708
Romario <uca8@mail.ru>
2019-05-03 00:43:01 +0300

    components

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+
+    <h2>Parent - {{ carName }}</h2>
+
+    <app-car
+            :carName="carName"
+            :carYear="carYear"
+            :counter="counter"
+            @nameChanged="carName = $event"
+            @counterUpdated="counter = $event"
+    >
+
+    </app-car>
+
+    <app-counter :counter="counter" ></app-counter>
+
+  </div>
+</template>
+
+<script>
+import Car from './Car.vue'
+import Counter from './Counter.vue'
+
+export default {
+  name: 'app',
+  components: {
+    appCar: Car,
+    appCounter: Counter
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      carName: "Mers",
+      carYear: 2017,
+      counter: 0
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/Car.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h3>
+           Name : {{ carName }}
+        </h3>
+        <p>Year : {{ carYear }}</p>
+        <button @click="changeName">Change Name</button>
+        <button @click="updateCounter">Update counter</button>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Car",
+        props: ['carName', 'carYear', 'counter'],
+        methods: {
+            changeName: function() {
+                this.carName = "Mazda";
+                this.$emit('nameChanged', this.carName);
+            },
+            updateCounter: function() {
+                this.$emit('counterUpdated', this.counter++);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
